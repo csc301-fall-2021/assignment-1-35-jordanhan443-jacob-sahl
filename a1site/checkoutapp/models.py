5b0f4a499eb212548c1f25a1db577d7ba30ee1ef
Jordan <jordannh25@gmail.com>
2021-10-04 23:40:24 -0400

    set up basic site

--- a1site/checkoutapp/models.py
+++ a1site/checkoutapp/models.py
@@ -1,3 +1,24 @@
+import uuid as uuid
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=100)
+    item_price = models.FloatField()
+
+    # a 13% tax -> tax_rate is 0.13
+    tax_rate = models.FloatField(default=0.13)
+
+    def __str__(self):
+        return self.item_name
+    """
+    def calculate_tax(self):
+        return self.item_price * self.tax_rate
+
+    def get_tax_rate(self):
+        return self.tax_rate
+
+    def calculate_total(self):
+        return self.item_price * (1 + self.tax_rate)
+    """
+
--- a1site/checkoutapp/views.py
+++ a1site/checkoutapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Item
+
+def index(request):
+    test_bool = False
+    context = {
+        'test_bool': test_bool,
+    }
+    return render(request, 'checkoutapp/index.html', context)
+
+def calculate(request):
+    pass
 
 # Create your views here.
