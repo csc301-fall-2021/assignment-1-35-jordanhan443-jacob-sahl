f9fd6ab90ba8e1b45a09843b20e1d24e3f6a0fe8
jacob-sahl <jacobsahlmueller@gmail.com>
2021-10-06 01:32:34 -0400

    Styling and basic add/remove functionality

--- a1site/frontend/src/App.js
+++ a1site/frontend/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     this.state = {
       viewCompleted: false,
       items: [],
+      cartItems: [],
     }
   }
 
@@ -30,34 +31,132 @@ class App extends Component {
     return this.setState({ viewCompleted: false });
   }
 
-  renderItems = () => {
+  renderInventory = () => {
     const items = this.state.items;
     return (
-      <table class="table table-striped table-hover">
-        <thead>
-          <tr>
-            <th scope="col">Item Name</th>
-            <th scope="col">Price</th>
-            <th scope="col">Tax Rate</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item) => (
+      <inventory>
+        <table-header>
+          <h1>Store Inventory</h1>
+          <h1>
+            <i class="fas fa-warehouse"></i>
+          </h1>
+        </table-header>
+        <table class="table table-striped table-dark table-hover">
+          <thead>
             <tr>
-              <th scope="row">{item.item_name}</th>
-              <td>{item.item_price}</td>
-              <td>{item.tax_rate}</td>
+              <th scope="col">Item Name</th>
+              <th scope="col">Price</th>
+              <th scope="col">Tax Rate</th>
+              <th scope="col"></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {items.map((item) => (
+              <tr>
+                <th scope="row">{item.item_name}</th>
+                <td>{item.item_price}</td>
+                <td>{item.tax_rate}</td>
+                <td>
+                  <button class="add" onClick={() => this.addItemToCart(item)}>
+                    <i class="far fa-plus-square"></i>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </inventory>
     )
   }
+
+  addItemToCart = (item) => {
+    const cart = this.state.cartItems;
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i].item === item) {
+        cart[i].count ++;
+        this.setState({ cartItems: cart })
+        return;
+      }
+    }
+    const new_item = {
+      item: item,
+      count: 1,
+    }
+    cart.push(new_item);
+    this.setState({ cartItems: cart })
+  }
+
+  removeItemFromCart = (item) => {
+    const cart = this.state.cartItems;
+    if (item.count > 1) {
+      cart[cart.indexOf(item)].count --;
+      this.setState({ cartItems: cart })
+    } else {
+      const new_cart = cart.filter((i) => {
+        return i !== item;
+      })
+      this.setState({ cartItems: new_cart })
+    }
+  }
+
+  renderCart = () => {
+    const cart = this.state.cartItems;
+    return (
+      <cart>
+        <table-header>
+          <h1>Your Shopping Cart</h1>
+          <h1>
+            <i class="fas fa-shopping-cart"></i>
+          </h1>
+        </table-header>
+        <table class="table table-striped table-dark table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Item Name</th>
+              <th scope="col">Price</th>
+              <th scope="col">Tax Rate</th>
+              <th scope="col">#</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item) => (
+              <tr>
+                <th scope="row">{item.item.item_name}</th>
+                <td>{item.item.item_price}</td>
+                <td>{item.item.tax_rate}</td>
+                <td>{item.count}</td>
+                <td>
+                  <button class="remove" onClick={() => this.removeItemFromCart(item)}>
+                    <i class="far fa-minus-square"></i>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {this.checkEmptyCart()}
+      </cart>
+    )
+  }
+
+  checkEmptyCart = () => {
+    if (this.state.cartItems.length === 0) {
+      return (
+        <empty-cart>
+          <p>
+            Your cart is empty
+          </p>
+        </empty-cart>
+      )
+    }
+  } 
 
   render() {
     return (
       <main>
-        {this.renderItems()}
+        {this.renderInventory()}
+        {this.renderCart()}
       </main>
     )
   }
