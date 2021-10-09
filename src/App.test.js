bb4f0993c662ca96984d0b5e3778dd41869a7121
jacob-sahl <jacobsahlmueller@gmail.com>
2021-10-08 20:44:48 -0400

    Got app working, had to rewrite some css

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import axios from 'axios';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+jest.mock('axios');
+
+describe('App works', () => {
+  test('app renders', () => {
+    render(<App />);
+    expect(screen.getByText('Store Inventory')).toBeInTheDocument();
+    expect(screen.getByText('Shopping Cart')).toBeInTheDocument();
+  });
+
+  test('items from database are added correctly', () => {
+    axios.get.mockResolvedValue({});
+  })
+
+
+})
